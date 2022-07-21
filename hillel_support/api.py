20914190f2e4e8fe27c6848212401ec6a863fe77
Tornesko <konstantinborovenskij@gmail.com>
2022-07-21 12:03:44 +0300

    commit

--- hillel_support/api.py
+++ hillel_support/api.py
@@ -5,16 +5,6 @@ from dataclasses import asdict, dataclass
 import requests
 from django.http import JsonResponse
 
-# import json
-# from django.http import HttpResponse
-# from http import HTTPStatus
-# def home(request):
-#     headers = {"Content-Type": "application/json"}
-#     # message = "{'message': 'hello'}"
-#     data = {"message": "hello"}
-#     message = json.dumps(data)
-#     return HttpResponse(message, headers=headers, status=HTTPStatus.OK)
-
 
 def home(request):
     data = {"message": "hello from json response", "num": 12.2}
